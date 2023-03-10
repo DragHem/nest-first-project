e04805249a33515c8ac30ea23da2ff31cf9f4e98
Damian Puś <pusiek99@interia.pl>
2023-03-10 14:21:54 +0100

    Added Shop Service and getProducts method

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { GetListOfProductsResponse } from '../interfaces/shop';
+import { ShopService } from './shop.service';
 
 @Controller('shop')
-export class ShopController {}
+export class ShopController {
+  constructor(@Inject(ShopService) private shopService: ShopService) {}
+  @Get('/')
+  getListOfProducts(): GetListOfProductsResponse {
+    return this.shopService.getProducts();
+  }
+}
---
+++ src/shop/shop.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { GetListOfProductsResponse } from '../interfaces/shop';
+
+@Injectable()
+export class ShopService {
+  getProducts(): GetListOfProductsResponse {
+    return [
+      {
+        name: 'iPhone',
+        price: 4200,
+        description: 'iPhone description',
+      },
+      {
+        name: 'Samsung FE2020',
+        price: 3600,
+        description: 'Samsung FE2020 description',
+      },
+      {
+        name: 'MacBook Air',
+        price: 4200,
+        description: 'MBA description',
+      },
+    ];
+  }
+}
